e958867902d2c7a4866f8c137a4faf73d0429414
GamerzinBR259 <108417553+GamerzinBR259@users.noreply.github.com>
2023-08-10 12:10:33 -0300

    Add files via upload

---
+++ menu2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/menu2.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Opa meu nobre</h1>
+
+<div id="pai"> <!--Início da div pai-->
+
+    <div class="filho"><img src="./img/nevoa.jpg" alt="nevoa"></div>
+
+    <div class="filho"><img src="./img/rari_pote.jpg" alt="pote"></div>
+    
+    <div class="filho"><img src="./img/escuros.jpg" alt="escuro"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div><!--Final da div pai-->
+
+</body>
+</html>
